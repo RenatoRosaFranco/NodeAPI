02befb65a2ea294c152d58ccd2e5cebae894b0b1
Renato Rosa Franco <Renato_ny@live.com>
2019-04-12 16:26:04 -0300

    Updated application

--- node-api/server.js
+++ node-api/server.js
@@ -3,6 +3,7 @@ const express    = require('express');
 const mongoose   = require('mongoose');
 const requireDir = require('require-dir');
 const app        = express();
+app.use(express.json());
 
 // Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
--- node-api/src/controllers/ProductController.js
+++ node-api/src/controllers/ProductController.js
@@ -1,9 +1,36 @@
 const mongoose = require('mongoose');
 const Product  = mongoose.model('Product');
 
+// ProductController
 module.exports = {
-  async index(req, res){
-    const products = await Product.find();
-    return res.json(products);
+
+  // GET /index
+  async index(req, res) {
+   const products = await Product.find();
+   return res.json(products);
+  },
+  
+  // POST /store
+  async store(req, res) {
+   const product = await Product.create(req.body);
+   return res.json(product);
+  },
+
+  // GET /show
+  async show(req, res) {
+    const product = Product.findById(req.params.id);
+    return res.json(product);
+  },
+
+  // PATCH /update
+  async update(req, res) {
+    const product = Product.findByIdAndUpdate(req.params.id, req.params.body, { new: true })
+    return res.json(product);
+  },
+  
+  // DELETE /destroy
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id);
+    return res.send('');
   }
 }
